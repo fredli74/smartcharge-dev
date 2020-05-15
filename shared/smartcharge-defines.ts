50053b267fc15d6c9398745d24b4b0430782b47e
Fredrik Lidström <fredli74@gmail.com>
2020-05-16 01:30:09 +0200

    Fixed defines / globals files

--- shared/smartcharge-defines.ts
+++ shared/smartcharge-defines.ts
@@ -5,11 +5,14 @@
  * @license MIT (MIT)
  */
 
+// Keeping globals file because I had problems using this .ts from vue.config.js
+const globals = require("./smartcharge-globals.json");
+export const API_PATH = globals.API_PATH;
+export const DEFAULT_PORT = globals.DEFAULT_PORT;
+
 export const PROJECT_NAME = "SmartCharge";
 export const PROJECT_VERSION = "1.0";
 export const PROJECT_AGENT = `${PROJECT_NAME}/${PROJECT_VERSION} (+...)`;
-export const API_PATH = "/api/gpl";
-export const DEFAULT_PORT = 3000;
 
 export const DEFAULT_LOCATION_RADIUS = 250; // Default geo fence radius in meters
 export const DEFAULT_DIRECTLEVEL = 15; // Default direct level if no setting for location
