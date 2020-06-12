74cf66865c330b7f0fd4cc87cf3da6fc96c32720
Fredrik Lidström <fredli74@gmail.com>
2020-06-12 21:32:58 +0200

    Work-around for missing legacy Model S images

--- providers/tesla/app/tesla-helper.ts
+++ providers/tesla/app/tesla-helper.ts
@@ -9,15 +9,28 @@ export interface TeslaNewListEntry {
   vehicle_uuid: string; // smartcharge vehicle_uuid
 }
 
-export function vehicleImage(vehicle: GQLVehicle, sideView?: boolean): string {
+export function vehicleImage(
+  vehicle: GQLVehicle,
+  sideView?: boolean,
+  dark?: boolean
+): string {
   let options = ["W38B", "PPSW"];
   if (vehicle.providerData.option_codes) {
     options = vehicle.providerData.option_codes;
   }
   let model = "m3";
-  const view = `STUD_${sideView ? "SIDE" : "3QTR"}`;
+  let background = 1;
   switch (vehicle.providerData.car_type) {
     case "models":
+      model = "msl";
+      if (dark) {
+        // There is no view with dark background
+        sideView = true;
+      } else {
+        // Force white background for legacy model images
+        background = 2;
+      }
+      break;
     case "models2":
       model = "ms";
       break;
@@ -28,6 +41,7 @@ export function vehicleImage(vehicle: GQLVehicle, sideView?: boolean): string {
       model = "my";
       break;
   }
+  const view = `STUD_${sideView ? "SIDE" : "3QTR"}`;
   const optionString = options.join(",");
-  return `https://static-assets.tesla.com/v1/compositor/?model=${model}&view=${view}&size=1440&options=${optionString}&bkba_opt=1&context=design_studio_2`;
+  return `https://static-assets.tesla.com/v1/compositor/?model=${model}&view=${view}&size=1440&options=${optionString}&bkba_opt=${background}&context=design_studio_2`;
 }
