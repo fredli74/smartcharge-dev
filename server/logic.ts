06810fc4cb1043ca008d5a275944dab7add69932
Fredrik Lidström <fredli74@gmail.com>
2020-06-20 13:48:13 +0200

    Handle when location is removed while connected to it

--- server/logic.ts
+++ server/logic.ts
@@ -147,11 +147,11 @@ export class Logic {
 
     if (data.connected || vehicle.connected_id) {
       // Are we connected?
-      let connection: DBConnected;
+      let connection: DBConnected | null;
       if (vehicle.connected_id === null) {
         // New connection, create a record
         doPricePlan = true;
-        connection = await this.db.pg.one(
+        connection = (await this.db.pg.one(
           `INSERT INTO connected($[this:name]) VALUES($[this:csv]) RETURNING *;`,
           {
             vehicle_uuid: input.id,
@@ -167,7 +167,7 @@ export class Logic {
             saved: 0,
             connected: true
           }
-        );
+        )) as DBConnected;
         vehicle.connected_id = connection.connected_id;
         log(
           LogLevel.Debug,
@@ -179,7 +179,7 @@ export class Logic {
         );
       } else {
         // or read charge record
-        connection = await this.db.pg.one(
+        connection = await this.db.pg.oneOrNone(
           `SELECT * FROM connected WHERE connected_id = $1`,
           [vehicle.connected_id]
         );
@@ -187,10 +187,10 @@ export class Logic {
 
       if (data.charging_to || vehicle.charge_id) {
         // Are we charging?
-        let charge: DBCharge;
+        let charge: DBCharge | null;
         if (vehicle.charge_id === null) {
           // New charge, create a charge record
-          charge = await this.db.pg.one(
+          charge = (await this.db.pg.one(
             `INSERT INTO charge($[this:name]) VALUES($[this:csv]) RETURNING *;`,
             {
               vehicle_uuid: input.id,
@@ -207,7 +207,7 @@ export class Logic {
               end_added: data.added,
               energy_used: 0
             }
-          );
+          )) as DBCharge;
           vehicle.charge_id = charge.charge_id;
           log(LogLevel.Debug, `Started charge ${charge.charge_id}`);
           await this.db.pg.none(
@@ -216,27 +216,28 @@ export class Logic {
           );
         } else {
           // or read charge record
-          charge = await this.db.pg.one(
+          charge = await this.db.pg.oneOrNone(
             `SELECT * FROM charge WHERE charge_id = $1`,
             [vehicle.charge_id]
           );
 
-          // Update charge record
-          const deltaChargeTime =
-            (now.getTime() - charge.end_ts.getTime()) / 1e3; // ms / 1000 = s
-          const deltaPowerUsed = Math.round(
-            Math.max(0, data.power * deltaChargeTime) / 60
-          ); // used = power (W) * time (s) = Ws / 60 = Wm
-          connection.energy_used += deltaPowerUsed;
+          if (connection && charge) {
+            // Update charge record
+            const deltaChargeTime =
+              (now.getTime() - charge.end_ts.getTime()) / 1e3; // ms / 1000 = s
+            const deltaPowerUsed = Math.round(
+              Math.max(0, data.power * deltaChargeTime) / 60
+            ); // used = power (W) * time (s) = Ws / 60 = Wm
+            connection.energy_used += deltaPowerUsed;
 
-          {
-            let cost = 0;
-            let saved = 0;
-            const priceLookup: {
-              price_now: number;
-              price_then: number;
-            } = await this.db.pg.one(
-              `WITH wouldve AS (
+            {
+              let cost = 0;
+              let saved = 0;
+              const priceLookup: {
+                price_now: number;
+                price_then: number;
+              } = await this.db.pg.one(
+                `WITH wouldve AS (
                   SELECT MIN(b.start_ts) as ts, SUM(a.end_ts - a.start_ts) as duration
                   FROM charge a JOIN connected b ON (a.connected_id = b.connected_id) WHERE b.connected_id = $1
               ), prices AS (
@@ -245,120 +246,121 @@ export class Logic {
               SELECT 
                   (SELECT price FROM prices WHERE ts < $3 ORDER BY ts DESC LIMIT 1) price_now,
                   (SELECT price FROM prices a, wouldve b WHERE a.ts < b.ts+duration ORDER BY a.ts DESC LIMIT 1) price_then;`,
-              [vehicle.connected_id, vehicle.location_uuid, new Date(now)]
-            );
+                [vehicle.connected_id, vehicle.location_uuid, new Date(now)]
+              );
 
-            if (priceLookup.price_now !== null) {
-              cost = Math.round(
-                (priceLookup.price_now * deltaPowerUsed) / 60e3
-              ); // price in (kWh) * used in (Wm)   Wm / 60e3 => kWh
+              if (priceLookup.price_now !== null) {
+                cost = Math.round(
+                  (priceLookup.price_now * deltaPowerUsed) / 60e3
+                ); // price in (kWh) * used in (Wm)   Wm / 60e3 => kWh
 
-              if (priceLookup.price_then !== null) {
-                saved = Math.round(
-                  ((priceLookup.price_then - priceLookup.price_now) *
-                    deltaPowerUsed) /
-                    60e3
-                );
+                if (priceLookup.price_then !== null) {
+                  saved = Math.round(
+                    ((priceLookup.price_then - priceLookup.price_now) *
+                      deltaPowerUsed) /
+                      60e3
+                  );
+                }
+              }
+
+              connection.cost += cost;
+              connection.saved += saved;
+
+              // Update stats with charge information
+              if (deltaChargeTime < statsDelta * 2) {
+                // Just a sanity check that we do not update stats with old data
+                statsData.charge_cost = cost;
+                statsData.charge_cost_saved = saved;
+                statsData.charged_seconds = statsDelta;
+                statsData.charge_energy = deltaPowerUsed;
               }
             }
 
-            connection.cost += cost;
-            connection.saved += saved;
-
-            // Update stats with charge information
-            if (deltaChargeTime < statsDelta * 2) {
-              // Just a sanity check that we do not update stats with old data
-              statsData.charge_cost = cost;
-              statsData.charge_cost_saved = saved;
-              statsData.charged_seconds = statsDelta;
-              statsData.charge_energy = deltaPowerUsed;
+            {
+              const update: any = {
+                end_ts: now,
+                end_level: data.level
+              };
+              if (data.charging_to) {
+                update.target_level = data.charging_to;
+                update.estimate = data.estimate;
+              }
+              if (data.connected) {
+                update.energy_used = charge.energy_used + deltaPowerUsed;
+                update.end_added = data.added;
+              }
+              log(
+                LogLevel.Debug,
+                `Updating charge ${vehicle.charge_id} with ${deltaPowerUsed} Wm used in ${deltaChargeTime}s`
+              );
+              charge = (await this.db.pg.one(
+                `UPDATE charge SET ($1:name) = ($1:csv) WHERE charge_id=$2 RETURNING *;`,
+                [update, vehicle.charge_id]
+              )) as DBCharge;
+              log(LogLevel.Trace, `charge: ${JSON.stringify(charge)}`);
             }
-          }
 
-          {
-            const update: any = {
-              end_ts: now,
-              end_level: data.level
-            };
-            if (data.charging_to) {
-              update.target_level = data.charging_to;
-              update.estimate = data.estimate;
-            }
-            if (data.connected) {
-              update.energy_used = charge.energy_used + deltaPowerUsed;
-              update.end_added = data.added;
-            }
-            log(
-              LogLevel.Debug,
-              `Updating charge ${vehicle.charge_id} with ${deltaPowerUsed} Wm used in ${deltaChargeTime}s`
-            );
-            charge = await this.db.pg.one(
-              `UPDATE charge SET ($1:name) = ($1:csv) WHERE charge_id=$2 RETURNING *;`,
-              [update, vehicle.charge_id]
-            );
-            log(LogLevel.Trace, `charge: ${JSON.stringify(charge)}`);
-          }
-
-          // charge_curve logic
-          if (data.level > charge.start_level) {
-            // Ignore first % since integer % lacks precision
-            const current: DBChargeCurrent | null = await this.db.pg.oneOrNone(
-              `SELECT * FROM charge_current WHERE charge_id = $1;`,
-              [vehicle.charge_id]
-            );
-            if (!current || data.level > current.start_level) {
-              // reached a new level, create charge curve statistics
-              doPricePlan = true;
-              if (current && data.level === current.start_level + 1) {
-                // only 1% changes, or we've been offline and it's unreliable
-                const duration =
-                  (now.getTime() - current.start_ts.getTime()) / 1e3; // ms / 1000 = s
-                const avgPower = arrayMean(current.powers); // Watt
-                const energyUsed = (avgPower * duration) / 60; // power (W) * time (s) = Ws / 60 = Wm
-                const energyAdded = data.added - current.start_added; // Wm
-                const avgTemp = arrayMean(current.outside_deci_temperatures); // deci-celsius
-                log(
-                  LogLevel.Debug,
-                  `Calculated charge curve between ${
-                    current.start_level
-                  }% and ${data.level}% is ${(energyUsed / 60.0).toFixed(
-                    2
-                  )}kWh in ${(duration / 60.0).toFixed(2)}m`
-                );
-                await this.db.setChargeCurve(
-                  input.id,
-                  vehicle.charge_id,
-                  current.start_level,
-                  duration,
-                  avgTemp,
-                  energyUsed,
-                  energyAdded
+            // charge_curve logic
+            if (data.level > charge.start_level) {
+              // Ignore first % since integer % lacks precision
+              const current: DBChargeCurrent | null = await this.db.pg.oneOrNone(
+                `SELECT * FROM charge_current WHERE charge_id = $1;`,
+                [vehicle.charge_id]
+              );
+              if (!current || data.level > current.start_level) {
+                // reached a new level, create charge curve statistics
+                doPricePlan = true;
+                if (current && data.level === current.start_level + 1) {
+                  // only 1% changes, or we've been offline and it's unreliable
+                  const duration =
+                    (now.getTime() - current.start_ts.getTime()) / 1e3; // ms / 1000 = s
+                  const avgPower = arrayMean(current.powers); // Watt
+                  const energyUsed = (avgPower * duration) / 60; // power (W) * time (s) = Ws / 60 = Wm
+                  const energyAdded = data.added - current.start_added; // Wm
+                  const avgTemp = arrayMean(current.outside_deci_temperatures); // deci-celsius
+                  log(
+                    LogLevel.Debug,
+                    `Calculated charge curve between ${
+                      current.start_level
+                    }% and ${data.level}% is ${(energyUsed / 60.0).toFixed(
+                      2
+                    )}kWh in ${(duration / 60.0).toFixed(2)}m`
+                  );
+                  await this.db.setChargeCurve(
+                    input.id,
+                    vehicle.charge_id,
+                    current.start_level,
+                    duration,
+                    avgTemp,
+                    energyUsed,
+                    energyAdded
+                  );
+                }
+                await this.db.pg.none(
+                  `INSERT INTO charge_current(charge_id) SELECT $[charge_id] WHERE NOT EXISTS (SELECT charge_id FROM charge_current WHERE charge_id = $[charge_id]);` +
+                    `UPDATE charge_current SET start_ts=$[start_ts], start_level=$[start_level], start_added=$[start_added], powers='{}', outside_deci_temperatures='{}' WHERE charge_id = $[charge_id];`,
+                  {
+                    charge_id: vehicle.charge_id,
+                    start_ts: now,
+                    start_level: data.level,
+                    start_added: data.added
+                  }
                 );
               }
-              await this.db.pg.none(
-                `INSERT INTO charge_current(charge_id) SELECT $[charge_id] WHERE NOT EXISTS (SELECT charge_id FROM charge_current WHERE charge_id = $[charge_id]);` +
-                  `UPDATE charge_current SET start_ts=$[start_ts], start_level=$[start_level], start_added=$[start_added], powers='{}', outside_deci_temperatures='{}' WHERE charge_id = $[charge_id];`,
+              const new_current = await this.db.pg.one(
+                `UPDATE charge_current SET powers = array_append(powers, $[power]), outside_deci_temperatures = array_append(outside_deci_temperatures, cast($[outside_temp] as smallint)) WHERE charge_id=$[charge_id] RETURNING *;`,
                 {
                   charge_id: vehicle.charge_id,
-                  start_ts: now,
-                  start_level: data.level,
-                  start_added: data.added
+                  power: Math.trunc(data.power),
+                  outside_temp: Math.trunc(data.outside_temp)
                 }
               );
+              log(LogLevel.Trace, `current: ${JSON.stringify(new_current)}`);
             }
-            const new_current = await this.db.pg.one(
-              `UPDATE charge_current SET powers = array_append(powers, $[power]), outside_deci_temperatures = array_append(outside_deci_temperatures, cast($[outside_temp] as smallint)) WHERE charge_id=$[charge_id] RETURNING *;`,
-              {
-                charge_id: vehicle.charge_id,
-                power: Math.trunc(data.power),
-                outside_temp: Math.trunc(data.outside_temp)
-              }
-            );
-            log(LogLevel.Trace, `current: ${JSON.stringify(new_current)}`);
           }
         }
 
-        if (!data.charging_to) {
+        if (!connection || !charge || !data.charging_to) {
           // We stopped charging
           log(LogLevel.Debug, `Ending charge ${vehicle.charge_id}`);
           await this.db.pg.none(
@@ -368,26 +370,28 @@ export class Logic {
         }
       }
 
-      // Update connection record
-      connection.end_ts = now;
-      connection.end_level = data.level;
-      await this.db.pg.one(
-        `UPDATE connected SET ($1:name) = ($1:csv) WHERE connected_id=$2 RETURNING *;`,
-        [
-          {
-            location_uuid: vehicle.location_uuid, // if a new location was added
-            end_ts: now,
-            end_level: data.level,
-            energy_used: connection.energy_used,
-            cost: connection.cost,
-            saved: connection.saved,
-            connected: Boolean(data.connected)
-          },
-          vehicle.connected_id
-        ]
-      );
+      if (connection) {
+        // Update connection record
+        connection.end_ts = now;
+        connection.end_level = data.level;
+        await this.db.pg.one(
+          `UPDATE connected SET ($1:name) = ($1:csv) WHERE connected_id=$2 RETURNING *;`,
+          [
+            {
+              location_uuid: vehicle.location_uuid, // if a new location was added
+              end_ts: now,
+              end_level: data.level,
+              energy_used: connection.energy_used,
+              cost: connection.cost,
+              saved: connection.saved,
+              connected: Boolean(data.connected)
+            },
+            vehicle.connected_id
+          ]
+        );
+      }
 
-      if (!data.connected) {
+      if (!connection || !data.connected) {
         // We disconnected
         log(
           LogLevel.Debug,
@@ -403,7 +407,7 @@ export class Logic {
           [vehicle.vehicle_uuid, ScheduleType.Manual]
         );
 
-        if (connection.location_uuid !== null) {
+        if (connection && connection.location_uuid !== null) {
           await this.createNewStats(vehicle, connection.location_uuid);
         }
       }
