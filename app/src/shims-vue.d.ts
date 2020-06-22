13139791cd88a6815b29dc5d0ef245932e8e8036
Fredrik Lidström <fredli74@gmail.com>
2020-06-22 23:51:15 +0200

    Removing ctk-date-time-picker

--- app/src/shims-vue.d.ts
+++ app/src/shims-vue.d.ts
@@ -3,9 +3,4 @@ declare module "*.vue" {
   export default Vue;
 }
 
-declare module "vue-ctk-date-time-picker" {
-  var ignoreTyping: any;
-  export = ignoreTyping;
-}
-
 declare module "vue-visible";
