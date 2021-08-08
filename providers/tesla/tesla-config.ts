4c322c39c0bd41a563bd6fc52de9988e5a50116a
Fredrik Lidström <fredli74@gmail.com>
2021-08-08 11:41:01 +0200

    Split Tesla owner and auth API to allow for different proxies

--- providers/tesla/tesla-config.ts
+++ providers/tesla/tesla-config.ts
@@ -1,18 +1,22 @@
 const config = {
   TESLA_API_BASE_URL: `https://owner-api.teslamotors.com/`,
   TESLA_API_PROXY: undefined,
-  TESLA_AUTH_BASE_URL: `https://auth.tesla.com/oauth2/v3/`,
-  TESLA_CLIENT_ID: `81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384`,
-  TESLA_CLIENT_SECRET: `c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3`,
   TESLA_API_HEADERS: {
+    "User-Agent": `curl/7.64.1`
+  },
+  // User agent was ${PROJECT_AGENT} ${provider.name}/${provider.version}`,
+  // then we had to use `curl/7.64.1`
+
+  TESLA_AUTH_BASE_URL: `https://auth.tesla.com/`,
+  TESLA_AUTH_PROXY: undefined,
+  TESLA_AUTH_HEADERS: {
     "User-Agent": `curl/7.64.1`
     //    "x-tesla-user-agent": `TeslaApp/3.10.9-433/adff2e065/android/10`,
     //    "X-Requested-With": "com.teslamotors.tesla"
   },
-  /*
-    User agent was ${PROJECT_AGENT} ${provider.name}/${provider.version}`,
-    then we had to use `curl/7.64.1`, but now we are fully faking it
-  */
+
+  TESLA_CLIENT_ID: `81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384`,
+  TESLA_CLIENT_SECRET: `c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3`,
   TOKEN_EXPIRATION_WINDOW: 300, // Pre-expiration Tesla API token renewal window
 
   TIME_BEFORE_TIRED: 20 * 60e3, // stay online 20 min after a drive or charge
