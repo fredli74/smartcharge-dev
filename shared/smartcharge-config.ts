297687e7972ae4b0d56280a0cbe0d7cd949f996e
Fredrik Lidström <fredli74@gmail.com>
2025-01-31 13:04:01 +0100

    Change default server port to 3030

--- shared/smartcharge-config.ts
+++ shared/smartcharge-config.ts
@@ -24,25 +24,21 @@ function getEnvVar(key: string, defaultValue: string) {
 
 
 // Config object
-const config = {
-  POSTGRES_DB: getEnvVar('POSTGRES_DB', `smartcharge`),
-  POSTGRES_USER: getEnvVar('POSTGRES_USER', `scserver`),
-  POSTGRES_PASSWORD: getEnvVar('POSTGRES_PASSWORD', `scserverpass`),
-  POSTGRES_HOST: getEnvVar('POSTGRES_HOST', `db`),
-  POSTGRES_PORT: getEnvVar('POSTGRES_PORT', `${5432}`),
-  POSTGRES_SSL: getEnvVar('POSTGRES_SSL', `${false}`),
-  POSTGRES_CONNECTIONS: getEnvVar('POSTGRES_CONNECTIONS', `${10}`),
-  PUBLIC_URL: getEnvVar('PUBLIC_URL', ``),
-  HELP_URL: getEnvVar('HELP_URL', `https://github.com/fredli74/smartcharge-dev/issues`),
-  SERVER_IP: getEnvVar('SERVER_IP', `0.0.0.0`),
-  SERVER_PORT: getEnvVar('SERVER_PORT', `${DEFAULT_PORT}`),
-  SINGLE_USER: getEnvVar('SINGLE_USER', `${true}`),
-  SINGLE_USER_PASSWORD: getEnvVar('SINGLE_USER_PASSWORD', `password`),
-  AUTH0_DOMAIN: getEnvVar('AUTH0_DOMAIN', `smartcharge.eu.auth0.com`),
-  AUTH0_CLIENT_ID: getEnvVar('AUTH0_CLIENT_ID', `WGPO7jrY6Sd0HcfkVvTetyrEnpsE8RQM`),
-  GLOBAL_INFO_MESSAGE: getEnvVar('GLOBAL_INFO_MESSAGE', ``),
-  GLOBAL_WARNING_MESSAGE: getEnvVar('GLOBAL_WARNING_MESSAGE', ``),
-  GLOBAL_ERROR_MESSAGE: getEnvVar('GLOBAL_ERROR_MESSAGE', ``),
+const config: ConfigType = {
+  POSTGRES_URL: process.env.POSTGRES_URL || `postgres://scserver:scserver@postgres:5432/smartcharge`,
+  POSTGRES_SSL: process.env.POSTGRES_SSL === "true",
+  POSTGRES_CONNECTIONS: parseInt(process.env.POSTGRES_CONNECTIONS || `10`),
+  PUBLIC_URL: process.env.PUBLIC_URL || ``,
+  HELP_URL: process.env.HELP_URL || `https://github.com/fredli74/smartcharge-dev/issues`,
+  SERVER_IP: process.env.SERVER_IP || `::`,
+  SERVER_PORT: parseInt(process.env.SERVER_PORT || `${DEFAULT_PORT}`),
+  SINGLE_USER: process.env.SINGLE_USER ? process.env.SINGLE_USER === "true" : true,
+  SINGLE_USER_PASSWORD: process.env.SINGLE_USER_PASSWORD || "password",
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN || `smartcharge.eu.auth0.com`,
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || `WGPO7jrY6Sd0HcfkVvTetyrEnpsE8RQM`,
+  GLOBAL_INFO_MESSAGE: process.env.GLOBAL_INFO_MESSAGE || ``,
+  GLOBAL_WARNING_MESSAGE: process.env.GLOBAL_WARNING_MESSAGE || ``,
+  GLOBAL_ERROR_MESSAGE: process.env.GLOBAL_ERROR_MESSAGE || ``,
 };
 
 export default config;
