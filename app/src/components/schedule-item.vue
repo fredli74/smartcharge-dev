f6057c5cae32ab6c353cb7510b1de9884434c444
Fredrik Lidström <fredli74@gmail.com>
2026-01-31 18:15:29 +0100

    Reworked level button and slider bindings
    - v-menu for level is now gated by localSchedule.level !== null, so “manual stop” shows no % or slider.
    - Added guards in cancelLevel/setLevel to no-op when level is null.
    - levelSlider initializes with this.schedule.level || 0 to ensure it's always a number.
    
    Signed-off-by: Fredrik Lidström <fredli74@gmail.com>

--- app/src/components/schedule-item.vue
+++ app/src/components/schedule-item.vue
@@ -40,6 +40,7 @@
         </v-col>
         <v-col cols="6" :sm="newSchedule ? '4' : '3'" class="text-right text-sm-left" order="3">
           <v-menu
+            v-if="localSchedule.level !== null"
             ref="levelMenu"
             v-model="levelMenu"
             :close-on-content-click="false"
@@ -50,7 +51,7 @@
             <template #activator="{ on }">
               <v-btn depressed class="px-2" v-on="on">
                 <v-icon left>mdi-lightning-bolt</v-icon>
-                {{ scheduleLevel }}%
+                {{ localSchedule.level }}%
               </v-btn>
             </template>
             <v-card>
@@ -160,7 +161,7 @@ export default class ScheduleItem extends Vue {
       timeMenu: false,
       levelMenu: false,
       capturing: false,
-      levelSlider: this.schedule.level,
+      levelSlider: this.schedule.level || 0,
 
       isSaving: false,
       isRemoving: false,
@@ -182,10 +183,12 @@ export default class ScheduleItem extends Vue {
     setTimeout(() => (this.capturing = false), 150);
   }
   cancelLevel() {
+    if (this.localSchedule.level == null) return;
     this.levelSlider = this.localSchedule.level;
     this.levelMenu = false;
   }
   async setLevel() {
+    if (this.localSchedule.level == null) return;
     this.localSchedule.level = this.levelSlider;
     this.levelMenu = false;
     if (!this.newSchedule) {
@@ -271,9 +274,6 @@ export default class ScheduleItem extends Vue {
     }
   }
 
-  get scheduleLevel(): number {
-    return this.localSchedule.level;
-  }
 }
 </script>
 <style>
